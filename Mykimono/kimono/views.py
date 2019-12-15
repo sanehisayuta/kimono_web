b74110ad8d08f95d484d63abe4e0c06d1e77dfbf
Yuta Sanehisa <s1f101703599@iniad.org>
2019-12-15 14:42:01 +0900

    3

--- Mykimono/kimono/views.py
+++ Mykimono/kimono/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return render(request, 'kimono/index.html', {})
+
+
+def add(request):
+    return render(request, 'kimono/edit.html', {})
+
+
+def edit(request, editing_id):
+    return render(request, 'kimono/edit.html', {})
+
+
+def delete(request):
+    return HttpResponse('Delete')
