a1b66751707c32bf05cf89af1994cb7d83967da5
Yuta Sanehisa <s1f101703599@iniad.org>
2019-12-17 16:16:52 +0900

    form fuyasu

--- Mykimono/kimono/forms.py
+++ Mykimono/kimono/forms.py
@@ -3,7 +3,13 @@ from django import forms
 
 class MessageForm(forms.Form):
     message = forms.CharField(
-        label='メッセージ',
+        label='firstname',
+        max_length=255,
+        required=True,
+        widget=forms.TextInput()
+    )
+    lastname = forms.CharField(
+        label='lastname',
         max_length=255,
         required=True,
         widget=forms.TextInput()
--- Mykimono/kimono/models.py
+++ Mykimono/kimono/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Message(models.Model):
     message = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
