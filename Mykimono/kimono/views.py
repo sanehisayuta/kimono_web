fed3cc71ca6986e668e398ff7e8f7c9f3687d794
Yuta Sanehisa <s1f101703599@iniad.org>
2019-12-17 15:39:18 +0900

    base template

--- Mykimono/kimono/views.py
+++ Mykimono/kimono/views.py
@@ -1,17 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from .models import Message
+from .forms import MessageForm
 # Create your views here.
 def index(request):
-    return render(request, 'kimono/index.html', {})
+    d = {
+    'messages':Message.objects.all(),
+    }
+    return render(request, 'kimono/index.html', d)
 
 
 def add(request):
-    return render(request, 'kimono/edit.html', {})
+    form = MessageForm(request.POST or None)
+    if form.is_valid():
+        Message.objects.create(**form.cleaned_data)
+        return redirect('index')
+
+    d = {
+        'form': form,
+    }
+    return render(request, 'kimono/edit.html', d)
 
 
 def edit(request, editing_id):
-    return render(request, 'kimono/edit.html', {})
+    message = get_object_or_404(Message, id=editing_id)
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message.message = form.cleaned_data['message']
+            message.save()
+            return redirect('index')
+    else:
+        form = MessageForm({'message': message.message})
+    d = {
+        'form': form,
+    }
+    return render(request, 'kimono/edit.html', d)
 
-
+@require_POST
 def delete(request):
-    return HttpResponse('Delete')
+    delete_ids = request.POST.getlist('delete_ids')
+    if delete_ids:
+        Message.objects.filter(id__in=delete_ids).delete()
+    return redirect('index')
