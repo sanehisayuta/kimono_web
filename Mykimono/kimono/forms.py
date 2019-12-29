89a84be9d262f04f5581300db87e7e89e7974b67
Yuta Sanehisa <s1f101703599@iniad.org>
2019-12-29 14:42:47 +0900

    form base

--- Mykimono/kimono/forms.py
+++ Mykimono/kimono/forms.py
@@ -4,12 +4,36 @@ from django import forms
 class MessageForm(forms.Form):
     message = forms.CharField(
         label='firstname',
-        max_length=255,
+        max_length=50,
         required=True,
         widget=forms.TextInput()
     )
     lastname = forms.CharField(
         label='lastname',
+        max_length=50,
+        required=True,
+        widget=forms.TextInput()
+    )
+    email = forms.CharField(
+        label='email',
+        max_length=50,
+        required=True,
+        widget=forms.TextInput()
+    )
+    mobile_number = forms.CharField(
+        label='Mobile number',
+        max_length=50,
+        required=True,
+        widget=forms.TextInput()
+    )
+    security_code = forms.CharField(
+        label='security_code',
+        max_length=5,
+        required=True,
+        widget=forms.TextInput()
+    )
+    free_zone = forms.CharField(
+        label='free_zone',
         max_length=255,
         required=True,
         widget=forms.TextInput()
--- Mykimono/kimono/models.py
+++ Mykimono/kimono/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Message(models.Model):
-    message = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)    
+    message = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    mobile_number = models.CharField(max_length=50)
+    security_code = models.CharField(max_length=5)
+    free_zone = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
